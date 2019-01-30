018a2c2893c8dc83885eb77962a76847d7f62746
renacin <renacin.matadeen@ryerson.ca>
2019-01-30 13:16:11 -0500

    Fixed Distance Issues, and Cleaned Code Up

--- functions.py
+++ functions.py
@@ -1,5 +1,5 @@
 # Name:                                             Renacin Matadeen
-# Date:                                                01/27/2018
+# Date:                                               01/30/2018
 # Title                                     Functions Used To Parse Data From GasBuddy
 #
 # ----------------------------------------------------------------------------------------------------------------------
@@ -9,8 +9,9 @@ import multiprocessing
 import pandas as pd
 import time
 # ----------------------------------------------------------------------------------------------------------------------
+
 # This Function Will Return A DF With Values That Are Within A Bounding Box
-def bounding_box_df(focus_lat, focus_long, pc_location_df, distance_):
+def bounding_box_df(focus_lat, focus_long, pc_df, distance_):
 
     # Find MIN/MAX Lat/Long
     lat_range = []
@@ -23,59 +24,62 @@ def bounding_box_df(focus_lat, focus_long, pc_location_df, distance_):
         lat_range.append(destination.latitude)
         long_range.append(destination.longitude)
 
-
-
-
-
-
-
     # Return A Version Of PC_Location_DF, That Extends Only To Identified Range
-
-    # The Point That Is The Closest Is Within Both Ranges
-    print(lat_range)
-    print(long_range)
-
-
-
-    pc_loc_lat = pc_location_df[pc_location_df['Latitude'].between(min(lat_range), max(lat_range), inclusive=True)]
-    pc_loc_ = pc_loc_lat[pc_loc_lat['Longitude'].between(min(long_range), max(long_range), inclusive=True)]
-
-    print(pc_loc_lat.describe())
-    print(pc_loc_.describe())
-    time.sleep(1000)
-
-
-
+    pc_loc_long = pc_df[pc_df['Longitude'].between(min(long_range), max(long_range), inclusive=True)]
+    pc_loc_ = pc_loc_long[pc_loc_long['Latitude'].between(min(lat_range), max(lat_range), inclusive=True)]
     return pc_loc_
 
 
 # This Function Will Find The Closest Postal Code To A Centroid, And Return A List That Matches The Index Of The Centroid File
-def find_closest_pc(centroid_df, pc_location_df, pc_name_df):
+def find_closest_pc(centroid_df, pc_df):
 
+    # Data To Be Collected
     closest_pc_list = []
+    focus_lat_list = []
+    focus_long_list = []
+
+    # Needed Hyperparametres
     centroid_df_len = len(centroid_df)
     counter_x = 1
-    distance_km = 2
+    initial_distance = 1.5
+    distance_km = initial_distance
 
     for index, row_c in centroid_df.iterrows():
         centroid_location = geopy.Point(row_c["Latitude"], row_c["Longitude"])
 
-        # Find Focus Values & Add Index As A Column
-        focus_df = bounding_box_df(row_c["Latitude"], row_c["Longitude"], pc_location_df, distance_km)
-        focus_df["Index_Values"] = focus_df.index.tolist()
+        # Keep Trying Until A Good Distance Value Is Found & Utilized
+        while True:
+            try:
+                # Find Focus Values & Add Index As A Column
+                focus_df = bounding_box_df(row_c["Latitude"], row_c["Longitude"], pc_df, distance_km)
+                focus_df["Index_Values"] = focus_df.index.tolist()
 
-        # Find Closest Postal Code To Focus Centroid
-        focus_df["D_To_C"] = focus_df.apply(lambda row: geodesic(centroid_location, geopy.Point(row["Latitude"], row["Longitude"])), axis=1)
-        val_list = focus_df["D_To_C"].tolist()
+                # Find Closest Postal Code To Focus Centroid
+                focus_df["D_To_C"] = focus_df.apply(lambda row: geodesic(centroid_location, geopy.Point(row["Latitude"], row["Longitude"])), axis=1)
+                val_list = focus_df["D_To_C"].tolist()
+
+                # If Everything Checks Out Break This While Loop
+                break
+
+            # If A Value Error Is Detected Iteratively Increase Distance By 1 KM Until Code Works
+            except ValueError:
+                distance_km += 3
+
+        # Reset The Value Of distance_km
+        distance_km = initial_distance
 
         # Find The Index Value & Convert To Postal Code
         min_index = int(val_list.index(min(val_list)))
         focus_df.reset_index(inplace = True)
         min_index_index_vals = focus_df["Index_Values"][min_index]
-        closest_pc_list.append(pc_name_df["PostalCode"][min_index_index_vals])
+
+        # Append The Needed Information
+        closest_pc_list.append(pc_df["PostalCode"][min_index_index_vals])
+        focus_lat_list.append(pc_df["Latitude"][min_index_index_vals])
+        focus_long_list.append(pc_df["Longitude"][min_index_index_vals])
 
         # Monitor Progress
-        print("Progress: " + str(counter_x) + "/" + str(centroid_df_len) + ", Postal Code: " + pc_name_df["PostalCode"][min_index] + ", Distance: " + str(min(val_list)))
+        print("Progress: " + str(counter_x) + "/" + str(centroid_df_len) + ", Postal Code: " + pc_df["PostalCode"][min_index_index_vals] + ", Distance: " + str(min(val_list)))
 
         # Del Column Just Created
         focus_df.drop(["D_To_C"], axis=1)
@@ -83,11 +87,11 @@ def find_closest_pc(centroid_df, pc_location_df, pc_name_df):
         # Add To Counter
         counter_x += 1
 
-        #time.sleep(20)
+    centroid_df["Closest_PC"] = closest_pc_list
+    centroid_df["P_Latitude"] = focus_lat_list
+    centroid_df["P_Longitude"] = focus_long_list
 
-    centroid_df["Closest_PC"] = closest_postal_code
+    # Save As CSV
+    centroid_df.to_csv(r"C:\Users\renac\Documents\Programming\Python\GasBuddy_DataParse\Data\GB_C_2KM\Closest_PC.csv", index=False)
 
 # ----------------------------------------------------------------------------------------------------------------------
-
-if __name__ == "__main__":
-    pass
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 # Name:                                             Renacin Matadeen
-# Date:                                                01/27/2018
+# Date:                                                01/30/2018
 # Title                                     Collecting Data From gasbuddy.com
 #
 # ----------------------------------------------------------------------------------------------------------------------
 from functions import find_closest_pc
 from geopy.distance import geodesic
-import multiprocessing
 import pandas as pd
 import time
 # ----------------------------------------------------------------------------------------------------------------------
@@ -13,6 +12,8 @@ import time
 # Import Centroid Data
 centroid_path = r"C:\Users\renac\Documents\Programming\Python\GasBuddy_DataParse\Data\GB_C_2KM\Spaced_Centroids.csv"
 centroid_data_df = pd.read_csv(centroid_path)
+
+centroid_data_df = centroid_data_df.copy()
 centroid_data_df['Latitude'] = centroid_data_df['Latitude'].astype('float32')
 centroid_data_df['Longitude'] = centroid_data_df['Longitude'].astype('float32')
 
@@ -20,65 +21,11 @@ centroid_data_df['Longitude'] = centroid_data_df['Longitude'].astype('float32')
 postal_path = r"C:\Users\renac\Documents\Programming\Python\GasBuddy_DataParse\Data\Postal_Codes\Ontario_PostalCodes.csv"
 postal_code_data_df = pd.read_csv(postal_path)
 
-# Focus On Postal Codes In Toronto Only | Make New Dataframe
-pc_values = postal_code_data_df.loc[postal_code_data_df["PostalCode"].str.startswith("M"), "PostalCode"].values
-lat_values = postal_code_data_df.loc[postal_code_data_df["PostalCode"].str.startswith("M"), "Latitude"].values
-long_values = postal_code_data_df.loc[postal_code_data_df["PostalCode"].str.startswith("M"), "Longitude"].values
-
-# Separate Coordinated & PostalCode Name
-data_lat_long = {"Latitude": lat_values, "Longitude": long_values}
-toronto_pc_locations_df = pd.DataFrame(data= data_lat_long)
-toronto_pc_locations_df['Latitude'] = toronto_pc_locations_df['Latitude'].astype('float32')
-toronto_pc_locations_df['Longitude'] = toronto_pc_locations_df['Longitude'].astype('float32')
-
-data_pc = {"PostalCode": pc_values}
-toronto_pc_df = pd.DataFrame(data= data_pc)
+postal_code_data_df = postal_code_data_df.copy()
+postal_code_data_df['Latitude'] = postal_code_data_df['Latitude'].astype('float32')
+postal_code_data_df['Longitude'] = postal_code_data_df['Longitude'].astype('float32')
 
 # ----------------------------------------------------------------------------------------------------------------------
 
 if __name__ == "__main__":
-    find_closest_pc(centroid_data_df, toronto_pc_locations_df, toronto_pc_df)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    # # Data For Chunking
-    # num_chunks = 3
-    # len_of_df = len(centroid_data_df)
-    # chunk_size = int(len_of_df / num_chunks)
-    # t_val = 0
-    # b_val = chunk_size
-    #
-    # # Split Centroid Dataframe Into Multiple Chunks
-    # for chunk in range(num_chunks):
-    #     exec("df_" + str(chunk + 1) + " = centroid_data_df[" + str(t_val) + ":" + str(b_val) + "]")
-    #     t_val += chunk_size
-    #     b_val += chunk_size
-    #
-    # # Initialize Multiprocessing Units
-    # for chunk in range(num_chunks):
-    #     exec("p_" + str(chunk + 1) + " = multiprocessing.Process(target=find_closest_pc, args=(" + "df_" + str(chunk + 1) + ", toronto_pc_locations_df, toronto_pc_df," + str(chunk + 1) + ")) ")
-    #
-    # # Start Multiprocessing
-    # for chunk in range(num_chunks):
-    #     exec("p_" + str(chunk + 1) + ".start()")
+    find_closest_pc(centroid_data_df, postal_code_data_df)
